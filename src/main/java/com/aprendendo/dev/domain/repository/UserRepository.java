d2807391f00a4561a47aafb701077402a0e0e595
Luis <luismiguelsantossouza@gmail.com>
2024-08-30 12:28:01 -0300

    add domain

--- src/main/java/com/aprendendo/dev/domain/repository/UserRepository.java
+++ src/main/java/com/aprendendo/dev/domain/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.aprendendo.dev.domain.repository;
 
-public interface UserRepository {
+import com.aprendendo.dev.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
